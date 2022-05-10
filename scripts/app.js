475cbbdfba274a48ad1bcd908e91d818701873df
drinos <nosi.sha@hotmail.com>
2022-05-10 17:26:37 +0200

    yes

---
+++ scripts/app.js
@@ -0,0 +1,226 @@
+"use strict";
+
+const menuHamburger = document.querySelector(".hamburger-menu");
+const menu = document.querySelector("header");
+const lines = document.querySelectorAll(".line");
+const mainLine = document.querySelector(".main-line");
+const secondaryLine = document.querySelector(".secondary-line");
+const thirdLine = document.querySelector(".third-line");
+
+menuHamburger.addEventListener("click", complicated);
+
+function complicated() {
+  menu.classList.toggle("header-show");
+  lines.forEach((curr, i) => curr.classList.toggle("line-show"));
+  mainLine.classList.toggle("main-line-clicked");
+  secondaryLine.classList.toggle("secondary-line-clicked");
+  thirdLine.classList.toggle("third-line-clicked");
+}
+
+////////////////////////////////////////////////////////////////
+
+const fadeEntry = document.querySelectorAll(".entry");
+
+// the options for the intersection observer
+const options = {
+  root: null,
+  threshold: 0.2,
+};
+
+// simple logic to add class when said divs are 20% visible on user's page
+const fadeFunction = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.add("entry-shown");
+
+  observer.unobserve(entry.target); // unobserve the divs at the end for added performance
+};
+
+const observer = new IntersectionObserver(fadeFunction, options);
+
+fadeEntry.forEach((curr, i) => observer.observe(curr)); // observing the divs with the intersection observer
+
+///////////////////////////////////////////////////////////
+
+const title = document.querySelector(".title"); // select the node
+const strTitle = title.textContent; // select the actual text so we have a string
+const titleSplit = strTitle.split("");
+title.textContent = "";
+
+for (let i = 0; i < titleSplit.length; i++) {
+  title.innerHTML += "<span>" + titleSplit[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 100);
+function onTick() {
+  const span = document.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  if (char === titleSplit.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer); // clear the interval
+  timer = null; // delete the timer function
+}
+
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////
+
+const ticking = document.querySelector(".ticking");
+const ticker = setInterval(function () {
+  ticking.classList.toggle("tick-active");
+}, 500);
+
+// Remove the ticker from view and use function "complete2"
+// to stop the ticking function from running forever
+const deleteTick = setTimeout(() => {
+  ticking.style.opacity = "0";
+  complete2();
+}, 8000);
+
+function complete2() {
+  clearInterval(ticker);
+}
+
+////////////////////////////////////////////////////////
+
+const clients = document.querySelector(".clients");
+const websites = document.querySelector(".websites");
+const exp = document.querySelector(".exp");
+
+const init = function () {
+  let counter1 = 0;
+  function client() {
+    if (counter1 <= 39) {
+      counter1++;
+      clients.innerHTML = `+${counter1}`;
+      ifComplete();
+    }
+  }
+
+  const ifComplete = function () {
+    if (counter1 === 39) {
+      complete3();
+    }
+  };
+
+  function complete3() {
+    clearInterval(client);
+    client = null;
+  }
+
+  const add = setInterval(client, 70);
+  //////////////////////////////////////////////////////////////
+  let counter2 = 0;
+
+  function client2() {
+    if (counter2 <= 99) {
+      counter2++;
+      websites.innerHTML = `+${counter2}`;
+      ifComplete2();
+    }
+  }
+
+  const ifComplete2 = function () {
+    if (counter2 === 99) {
+      complete4();
+    }
+  };
+
+  function complete4() {
+    clearInterval(client2);
+    client = null;
+  }
+
+  const add2 = setInterval(client2, 20);
+
+  ///////////////////////////////////////////////////
+  let counter3 = 0;
+
+  function client3() {
+    if (counter3 <= 2) {
+      counter3++;
+      exp.innerHTML = `+${counter3}`;
+      ifComplete3();
+    }
+  }
+
+  const ifComplete3 = function () {
+    if (counter3 === 2) {
+      complete5();
+    }
+  };
+
+  function complete5() {
+    clearInterval(client3);
+    client = null;
+  }
+
+  const add3 = setInterval(client3, 200);
+};
+
+//////////////////////////////////////////////////////////////////
+
+const numSection = document.querySelector(".section-Numbers");
+
+const options2 = {
+  threshold: 0.5,
+  root: null,
+};
+
+const initialization = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  init();
+  observer.unobserve(entry.target);
+};
+
+const observer2 = new IntersectionObserver(initialization, options2);
+
+observer2.observe(numSection);
+
+/////////////////////////////////////////////////////////////////
+
+const items = document.querySelector(".items");
+
+const newoptions = {
+  root: null,
+  threhold: 0.1,
+};
+
+const fadeWork = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.add("items-active");
+};
+
+const newObserver = new IntersectionObserver(fadeWork, newoptions);
+
+newObserver.observe(items);
+
+/////////////////////////////////////
+
+const aClick = document.querySelector(".my-work");
+const aClick2 = document.querySelector(".home");
+const work = document.querySelector("#work");
+const home = document.querySelector("#home");
+
+aClick.addEventListener("click", function (event) {
+  event.preventDefault();
+  complicated();
+  work.scrollIntoView({ behavior: "smooth" });
+});
+
+aClick2.addEventListener("click", function (event) {
+  event.preventDefault();
+  complicated();
+  home.scrollIntoView({ behavior: "smooth" });
+});
+
+//////////////////////////////////////////////////
